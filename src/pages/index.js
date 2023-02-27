add84b32fd2b0024e1de8201080c9c6432aaf367
Riyaz <34576223+riyazpt@users.noreply.github.com>
2023-02-27 10:45:21 +0400

    Case sensitivity next/link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 
 
-import Link from 'next/Link';
+import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import { fetchApi,baseUrl } from 'utils/fetchApi';
