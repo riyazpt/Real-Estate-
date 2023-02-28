2c6cb292fd9ac51067c0bfc879e67d0d8216d24c
Riyaz <34576223+riyazpt@users.noreply.github.com>
2023-02-28 09:33:59 +0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react';
 import Router from 'next/router';
 import Head from 'next/head';
-import { NProgress } from 'nprogress';
+import  NProgress  from 'nprogress';
 import Layout from '../../componets/Layout';
 
 export default function App({ Component, pageProps }) {
